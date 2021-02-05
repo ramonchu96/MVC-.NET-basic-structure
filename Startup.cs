956319fc65fbd489415113951ce75c44e02bf3ff
S$Code <57636365+ramonchu96@users.noreply.github.com>
2021-02-05 10:58:16 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationPracticaUD6Sesion1Guiada.Data;
+
+namespace WebApplicationPracticaUD6Sesion1Guiada
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Esta línea es sinónimo de AddMVC();
+            services.AddControllersWithViews();
+
+            //Añadimos un StudentRespository al sistema para que por inyección
+            // de dependencias se inserte en el controlador StudentRespository
+            services.AddScoped<StudentRepository>(_ => new StudentRepository());
+
+            services.AddTransient<StudentRepositoryDb>(_ => new StudentRepositoryDb(Configuration["ConnectionStrings:DefaultConnectionStudentDb"]));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
